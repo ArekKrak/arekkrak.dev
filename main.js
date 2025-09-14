a945afffccd76ca14a9c445d26cb9d0a217e3c7c
Arek Krakowiak <369arek12@protonmail.com>
2025-09-14 06:39:37 +0100

    debug errors

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 // Select the container that holds both images
 const portrait = document.querySelector('.main-image');
 
-// Preload the medieval image to avoid flicker on first hover/click
+if (portrait) {
+    // Preload the medieval image to avoid flicker on first hover/click
+    const preload = new Image();
+    preload.src = 'img/medieval-style.png'
 
-const preload = new Image();
-preload.src = 'img/medieval-style.png'
+    /* Function to toggle the "flipped" class on .main-image
+        - If .flipped is present - medieval image is shown
+        - If .flipped is removed - modern image is shown */
+    function toggleQuick() {
+        portrait.classList.toggle('flipped');
+    }
 
-/* Function to toggle the "flipped" class on .main-image
-    - If .flipped is present - medieval image is shown
-    - If .flipped is removed - modern image is shown */
-function toggleQuick() {
-    portrait.classList.toggle('flipped');
+    /* Event listener for mouse click on the portrait
+        - Adds/removes the "flipped" class on click */
+    portrait.addEventListener('click', toggleQuick);
 }
-
-/* Event listener for mouse click on the portrait
-    - Adds/removes the "flipped" class on click */
-portrait.addEventListener('click', toggleQuick);
-
 /* ================================================== */
 /* === ROTATING BACKGROUND THAT TRACKS DARK/LIGHT === */
 
